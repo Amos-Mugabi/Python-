87ff377634364d043659d9e31b21f3ac84f362c8
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-05-03 17:26:20 +0300

    Add files via upload

---
+++ Replitday23.py
@@ -0,0 +1,15 @@
+
+print("REPLIT LOGIN SYSTEM")
+print()
+
+while True:
+  name = input("What is your username: ")
+  password = input("What is your password: ")
+  if name == "John" and password == "j123":
+    print("Welcome to Replit!")
+    print("Code it. Have fun!")
+    break
+  else:
+    print("Sorry, try again!")
+    continue
+    
