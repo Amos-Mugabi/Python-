a605b5058c154c08ee3e84d18eb216105f068f3d
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-05-17 15:21:32 +0300

    Add files via upload

---
+++ Replitday30.py
@@ -0,0 +1,14 @@
+print("30 Days Down")
+print()
+
+
+for i in range(1,8):
+  thought = input(f"Day {i}:\n")
+  print()
+  response = f"You thought Day {i} was"
+  print(f"{response: ^35}")
+  print(f"{thought:^35}")
+  print()
+  
+
+
